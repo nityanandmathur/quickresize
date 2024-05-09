6e324267a1f348f484482588a0d6472178114e26
nityanandmathur <nityanandmathur@gmail.com>
2024-05-09 17:23:29 +0530

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ readme = open('README.md').read()
 setup(
     name='quickresize',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.2.0',
     description='In-place resizing of images present in a folder',
     long_description=readme,
     long_description_content_type='text/markdown',
